2dfacc52ee0aa01f9c846b1fc991ea06489183d6
AlbinaKost <143324469+AlbinaKost@users.noreply.github.com>
2024-11-09 16:30:09 +0200

    fixed localStorage and header

--- components/Products/Products.js
+++ components/Products/Products.js
@@ -1,24 +1,61 @@
 class Products {
-  render() {
-    let htmlCatalog = "";
-    CATALOG.forEach(({ id, name, image, price }) => {
-      htmlCatalog += `
-        <li class="products-element">
-          <span class="products-element_name">${name}</span>
-            <img class="products-element_image" src="${image}" />
-            <span class="products-element_price">⚡️${price.toLocaleString()} USD</span>        
-            <button class="products-element_button">Add to cart</button>                
+  constructor() {
+    this.classNameActive = "products-element_btn_active";
+    this.labelAdd = "Add to cart";
+    this.labelRemove = "Remove";
+  }
 
-          </li>
-        `;
+  handleSetLocationStorage(element, id) {
+    const { pushProduct, products } = localStorageUtil.putProducts(id);
+
+    if (pushProduct) {
+      element.classList.add(this.classNameActive);
+      element.innerHTML = this.labelRemove;
+    } else {
+      element.classList.remove(this.classNameActive);
+      element.innerHTML = this.labelAdd;
+    }
+    headerPage.render(products.length);
+  }
+
+  render() {
+    const productsStore = localStorageUtil.getProducts();
+    let htmlCatalog = "";
+
+    CATALOG.forEach(({ id, name, price, image }) => {
+      let activeClass = "";
+      let activeText = "";
+
+      if (productsStore.indexOf(id) === -1) {
+        activeText = this.labelAdd;
+      } else {
+        activeClass = " " + this.classNameActive;
+        activeText = this.labelRemove;
+      }
+
+      htmlCatalog += `
+                <li class="products-element">
+                    <span class="products-element_name">${name}</span>
+                    <img class="products-element_image" src="${image}" />
+                    <span class="products-element_price">
+                        ⚡️ ${price.toLocaleString()} USD
+                    </span>
+                    <button class="products-element_btn${activeClass}" onclick="productsPage.handleSetLocationStorage(this, '${id}');">
+                        ${activeText}
+                    </button>
+                </li>
+            `;
     });
+
     const html = `
-        <ul class="products-container">
-          ${htmlCatalog}
-        </ul>
-      `;
+            <ul class="products-container">
+                ${htmlCatalog}
+            </ul>
+        `;
+
     ROOT_PRODUCTS.innerHTML = html;
   }
 }
+
 const productsPage = new Products();
 productsPage.render();
